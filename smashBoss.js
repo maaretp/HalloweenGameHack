b7b55264d9ec10d6b2e16791c089b751aec2217d
maaretp <maaret@iki.fi>
2015-10-31 18:10:29 +0200

    spike wall works and score gets updates

--- smashBoss.js
+++ smashBoss.js
@@ -1,46 +1,47 @@
 //canvas Element
  Crafty.init(750,500, document.getElementById('game'));
 
-
+//placeholder for resizing images
 //Crafty.sprite("pumpkin.png", {bossImage:[10,390,100,100]});
 //var boss_entity = Crafty.e("2D, DOM, bossImage");
 
-var score = 100;
+var score = 0;
 
-//Score Box
+// Score Box
 var scoreBox = Crafty.e("2D, DOM, Canvas, Color, Text")
 .color('white')
 .textFont({ size: '20px', weight: 'bold' })
 .attr({ x: 600, y: 50})
 .text("Score: " + score);
 
-
-
 // Walls
- var wallBottom = Crafty.e('wallBottom, 2D, Canvas, Color')
+ var wallBottom = Crafty.e('wallBottom, 2D, Canvas, Color, wall')
   .attr({x: 0, y: 490, w: 750, h: 10})
   .color('blue');
 
-var wallTop = Crafty.e('wallTop, 2D, Canvas, Color, Collision')
+var wallTop = Crafty.e('wallTop, 2D, Canvas, Color, Collision, wall')
    .attr({x: 0, y: 0, w: 750, h: 10})
    .color('green')
    .collision();
 
-var wallLeft = Crafty.e('wallLeft, 2D, Canvas, Color')
+var wallLeft = Crafty.e('wallLeft, 2D, Canvas, Color, wall')
     .attr({x: 0, y: 0, w: 10, h: 500})
     .color('brown');
 
-var wallRight = Crafty.e('wallRight, 2D, Canvas, Color, Image')
+var wallRight = Crafty.e('wallRight, 2D, Canvas, Color, wall')
      .attr({x: 740, y: 0, w: 10, h: 500})
      .color('yellow')
-     .image('1wall.png');
 
-//Character Element moves Fourway
+var spikeWall = Crafty.e('spikeWall, 2D, Canvas, Color, Image')
+          .attr({x: 730, y: 0, w: 10, h: 500})
+          .image('img/1wall.png');
+
+// Character Element moves Fourway
  var boss = Crafty.e('boss, 2D, DOM, Color, Fourway, Collision, Image')
    .attr({x: 10, y: 390, w: 100, h: 100})
    .color('red')
    .fourway(4)
-   .image('pic.png');
+   .image('img/pic.png');
 
    /*boss.addComponent("Collision").bind('Moved', function(from) {
      if(this.hit('2D')) {
@@ -59,14 +60,32 @@ var wallRight = Crafty.e('wallRight, 2D, Canvas, Color, Image')
 
    /* Collision Code */
    //now either solid object or noticing the collision, needs fixing
-  boss.addComponent("Collision")
+  /*boss.addComponent("Collision")
   .checkHits('wallTop') // check for collisions with entities that have the Solid component in each frame
   .bind("HitOn", function(hitData) {
      console.log(hitData);
      console.log(this);
      console.log("Collision with Solid entity occurred for the first time.");
+
+
  }).bind("HitOff", function(comp) {
        //this.attr({x: hitData.x, y:hitData.y});
       console.log(comp);
         console.log("Collision with Solid entity ended.");
     });
+*/
+
+boss.addComponent("Collision")
+.bind('Moved', function(from) {
+  if(this.hit('wall')) {
+     this.attr({x: from.x, y:from.y});
+  }
+}).checkHits('spikeWall') // check for collisions with entities that have the Solid component in each frame
+.bind("HitOn", function(hitData) {
+   console.log("Collision with Solid entity occurred for the first time.");
+   score = score + 10;
+
+   console.log(score);
+
+   scoreBox.text("Score: " + score);
+});
